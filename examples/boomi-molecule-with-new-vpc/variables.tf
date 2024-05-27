7b8cf02977f5f1f159d48ff80306ce54103c2baa
manasa-nm <manasa.narasimhamurthy@boomi.com>
2024-05-27 12:40:25 +0530

    default var update

--- examples/boomi-molecule-with-new-vpc/variables.tf
+++ examples/boomi-molecule-with-new-vpc/variables.tf
@@ -2,7 +2,7 @@
 variable deployment_name {
     description = "Deployment Name for the boomi terraform deployment."
     type = string
-    default = "boomi-eks-blueprint-09713"
+    default = "boomi-eks-blueprint-013"
 }
 variable region {
     type = string
